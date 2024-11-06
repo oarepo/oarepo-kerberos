1c3c7db95a2032ac3b83b66e28d7b6c391c07264
senyaaa <pogodars@cvut.cz>
2024-11-06 20:58:50 +0100

    cli mapping add done and small refactoring

--- oarepo_kerberos/cli/cli.py
+++ oarepo_kerberos/cli/cli.py
@@ -1,6 +1,8 @@
 import click
 from invenio_db import db
 from invenio_accounts.models import UserIdentity, User
+from invenio_accounts.errors import AlreadyLinkedError
+
 
 """
 invenio kerberos mapping add <email> <kerberos id>
@@ -20,25 +22,44 @@ def mapping():
 @click.argument('email')
 @click.argument('kerberos_id')
 def add_mapping(email, kerberos_id):
-    # TODO
-    click.echo(f"Mapping added: {email} -> {kerberos_id}")
+    """Add kerberos mapping."""
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        click.echo(f"Error: User with email {email} not found.")
+        return
+
+    existing_mapping = UserIdentity.get_user(method="kerberos",external_id=kerberos_id)
+    if existing_mapping:
+        click.echo(f"Error: Mapping to kerberos {kerberos_id} already exists.")
+        return
+
+    try:
+       UserIdentity.create(user=user, method="kerberos", external_id=kerberos_id)
+       db.session.commit()
+       click.echo(f"Mapping added: {email} -> {kerberos_id}")
+    except AlreadyLinkedError:
+       click.echo(f"Error: Already linked.")
+    except Exception as e:
+       click.echo(f'Error: {e}')
+
 
 @mapping.command('remove')
 @click.argument('email')
 @click.argument('kerberos_id')
 def remove_mapping(email, kerberos_id):
     """Remove a Kerberos mapping."""
-    user = User.query.filter_by(_email=email).first()
+    user = User.query.filter_by(email=email).first()
 
     if not user:
-        click.echo(f"User with email {email} not found.")
+        click.echo(f"Error: User with email {email} not found.")
         return
 
-    deleted_rows = UserIdentity.query.filter_by(id=kerberos_id,id_user=user.id).delete()
+    UserIdentity.delete_by_external_id(method="kerberos", external_id=kerberos_id)
+    db.session.commit()
 
-    #db.session.commit()
-
-    if deleted_rows > 0:
+    deleted_rows = UserIdentity.query.filter_by(id=kerberos_id, id_user=user.id, method="kerberos").count()
+    if deleted_rows == 0:
         click.echo(f"Mapping removed: {email} -> {kerberos_id}")
     else:
         click.echo(f"No mapping found for {email} with Kerberos ID {kerberos_id}.")
