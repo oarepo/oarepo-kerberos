73af80c960821d3a4e434aa3ae0b6068b7292009
senyaaa <pogodars@cvut.cz>
2024-11-06 20:15:02 +0100

    cli mapping remove done

--- oarepo_kerberos/cli/cli.py
+++ oarepo_kerberos/cli/cli.py
@@ -28,18 +28,30 @@ def add_mapping(email, kerberos_id):
 @click.argument('kerberos_id')
 def remove_mapping(email, kerberos_id):
     """Remove a Kerberos mapping."""
-    # TODO
-    click.echo(f"Mapping removed: {email} -> {kerberos_id}")
+    user = User.query.filter_by(_email=email).first()
+
+    if not user:
+        click.echo(f"User with email {email} not found.")
+        return
+
+    deleted_rows = UserIdentity.query.filter_by(id=kerberos_id,id_user=user.id).delete()
+
+    #db.session.commit()
+
+    if deleted_rows > 0:
+        click.echo(f"Mapping removed: {email} -> {kerberos_id}")
+    else:
+        click.echo(f"No mapping found for {email} with Kerberos ID {kerberos_id}.")
 
 @mapping.command('get')
 @click.option('--email', default=None, help="Filter by email.")
 def get_mapping(email):
     """List Kerberos mappings."""
-    query = db.session.query(User._email, UserIdentity.id).join(
+    query = db.session.query(User.email, UserIdentity.id).join(
         UserIdentity, User.id == UserIdentity.id_user
     )
     if email:
-        query = query.filter(User._email == email)
+        query = query.filter(User.email == email)
 
     results = query.all()
 
