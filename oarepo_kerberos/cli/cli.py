da6cd8ee87d49641243c6606cdb25f11099f1ab8
senyaaa <pogodars@cvut.cz>
2024-11-21 19:24:15 +0100

    cli format fixes

--- oarepo_kerberos/cli/cli.py
+++ oarepo_kerberos/cli/cli.py
@@ -1,27 +1,34 @@
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-kerberos is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Oarepo kerberos command line tool implementation."""
+
+from __future__ import annotations
+
 import click
-from invenio_db import db
-from invenio_accounts.models import UserIdentity, User
 from invenio_accounts.errors import AlreadyLinkedError
+from invenio_accounts.models import User, UserIdentity
+from invenio_db import db
 
-
-"""
-invenio kerberos mapping add <email> <kerberos id>
-invenio kerberos mapping remove <email> <kerberos id>
-invenio kerberos mapping get [<email>] → vylistuje email, kerberos id
-"""
 
 @click.group()
-def kerberos():
+def kerberos() -> None:
     """Kerberos commands."""
 
+
 @kerberos.group()
-def mapping():
+def mapping() -> None:
     """Manage Kerberos mappings."""
 
-@mapping.command('add')
-@click.argument('email')
-@click.argument('kerberos_id')
-def add_mapping(email, kerberos_id):
+
+@mapping.command("add")
+@click.argument("email")
+@click.argument("kerberos_id")
+def add_mapping(email: str, kerberos_id: str) -> None:
     """Add kerberos mapping."""
     user = User.query.filter_by(email=email).first()
 
@@ -30,25 +37,27 @@ def add_mapping(email, kerberos_id):
         return
 
     realm = kerberos_id.split("@")[-1]
-    existing_mapping = UserIdentity.get_user(method=f"krb-{realm}",external_id=kerberos_id)
+    existing_mapping = UserIdentity.get_user(
+        method=f"krb-{realm}", external_id=kerberos_id
+    )
     if existing_mapping:
         click.echo(f"Error: Mapping to kerberos {kerberos_id} already exists.")
         return
 
     try:
-       UserIdentity.create(user=user, method=f"krb-{realm}", external_id=kerberos_id)
-       db.session.commit()
-       click.echo(f"Mapping added: {email} -> {kerberos_id}")
+        UserIdentity.create(user=user, method=f"krb-{realm}", external_id=kerberos_id)
+        db.session.commit()
+        click.echo(f"Mapping added: {email} -> {kerberos_id}")
     except AlreadyLinkedError:
-       click.echo(f"Error: Already linked.")
+        click.echo("Error: Already linked.")
     except Exception as e:
-       click.echo(f'Error: {e}')
+        click.echo(f"Error: {e}")
 
 
-@mapping.command('remove')
-@click.argument('email')
-@click.argument('kerberos_id')
-def remove_mapping(email, kerberos_id):
+@mapping.command("remove")
+@click.argument("email")
+@click.argument("kerberos_id")
+def remove_mapping(email: str, kerberos_id: str) -> None:
     """Remove a Kerberos mapping."""
     user = User.query.filter_by(email=email).first()
 
@@ -62,9 +71,10 @@ def remove_mapping(email, kerberos_id):
 
     click.echo(f"Mapping removed: {email} -> {kerberos_id}")
 
-@mapping.command('get')
-@click.option('--email', default=None, help="Filter by email.")
-def get_mapping(email):
+
+@mapping.command("get")
+@click.option("--email", default=None, help="Filter by email.")
+def get_mapping(email: str | None) -> None:
     """List all Kerberos mappings or specific user mapping."""
     query = db.session.query(User.email, UserIdentity.id).join(
         UserIdentity, User.id == UserIdentity.id_user
