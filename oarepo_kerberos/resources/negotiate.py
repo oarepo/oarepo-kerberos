7c08f51ec8b1f67b6246432f0d3cb2b9312b7010
senyaaa <pogodars@cvut.cz>
2024-11-21 19:25:59 +0100

    resources format fixes

--- oarepo_kerberos/resources/negotiate.py
+++ oarepo_kerberos/resources/negotiate.py
@@ -1,29 +1,48 @@
-from flask_resources import HTTPJSONException
-from flask_login import current_user
+#
+# Copyright (C) 2024 CESNET z.s.p.o.
+#
+# oarepo-kerberos is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+#
+"""Custom HTTP Response for Negotiating authentication."""
+
+from __future__ import annotations
+
 import json
+from typing import Any, Optional
+
+from flask_login import current_user
+from flask_resources import HTTPJSONException
+
 
 class NegotiateAuthentication(HTTPJSONException):
-    def __init__(self,*args, **kwargs):
+    """Custom HTTP exception for Negotiate authentication."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the NegotiateAuthentication exception."""
         if 403 in args:
             self.code = 403
-            self.description = 'You do not have permission to access this resource.'
+            self.description = "You do not have permission to access this resource."
         elif current_user.is_authenticated:
             self.code = 200
-            self.description = 'Authenticated successfully.'
+            self.description = "Authenticated successfully."
         else:
             self.code = 401
-            self.description = 'Authentication is required to access this resource.'
+            self.description = "Authentication is required to access this resource."
 
         super().__init__(**kwargs)
 
-
-    def get_headers(self, environ=None, scope=None):
+    def get_headers(
+        self, environ: Optional[dict] = None, scope: Optional[dict] = None
+    ) -> list[tuple[str, str]]:
+        """Get the HTTP headers for the response."""
         return [("WWW-Authenticate", "Negotiate")]
 
-    def get_body(self, environ=None, scope=None):
-        body = {
-            "status": self.code,
-            "message": self.get_description(environ)
-        }
+    def get_body(
+        self, environ: Optional[dict] = None, scope: Optional[dict] = None
+    ) -> str:
+        """Get the HTTP body for the response."""
+        body = {"status": self.code, "message": self.get_description(environ)}
 
         return json.dumps(body)
